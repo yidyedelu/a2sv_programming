872c5f80ab7308bcea4f201e98539f60bb7b7448
yidyedelu <90923397+yidyedelu@users.noreply.github.com>
2023-02-03 15:11:06 +0300

    added zigzag-conversion.cpp

---
+++ zigzag-conversion.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string convert(string const& s, int numRows) {
+        bool down = true;
+        int row = 0;
+        if (numRows == 1){
+            return s;
+        }
+        vector<string> zig(numRows, "");
+        for(int i = 0; i < s.size(); i++){
+            zig[row].push_back(s[i]);
+            if (row == numRows-1) down = false;
+            else if(row == 0) down = true;
+            if(down) row++;
+            else row--;
+        }
+        string p = "";
+        p.reserve(s.length());
+        for(int i = 0; i < zig.size();i++){
+            p += zig[i];
+        }
+        return p;
+
+    }
+};
